baa3874204c821aa0004675f1d0ae84d7f87c705
Tr4shB1t <tr4shb1t@gmail.com>
2025-12-05 16:51:26 +0100

    ball bewegung jetzt mit pygame vector funktion und mit abprallwinkeln

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pygame, random
 
+Vector2 = pygame.math.Vector2
 pygame.init()
 
 # Set up display
@@ -16,14 +17,14 @@ game_speed_delay = 15
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Ping Pong")
 
+ball = pygame.Rect((WIDTH - ball_size) // 2, (HEIGHT - ball_size) // 2, ball_size, ball_size)
+ball_vel = Vector2(1, 0).rotate(random.uniform(-45, 45)) * 5
+
 player1_pos = (HEIGHT - player1_hight) // 2  # Initial position of the square
 player2_pos = (HEIGHT - player2_hight) // 2  # Initial position of the square
 
-ball_pos_x = (WIDTH - ball_size) // 2 # Initial position of the ball
-ball_pos_y = (HEIGHT - ball_size) // 2
-
-ball_direction_x = random.choice([-4, 4])
-ball_direction_y = random.choice([-4, 4])
+player1 = pygame.Rect(border_left_right, player1_pos, player_thickness, player1_hight)
+player2 = pygame.Rect(WIDTH - border_left_right - player_thickness, player2_pos, player_thickness, player2_hight)
 
 font = pygame.font.SysFont('Courier New', 50, bold=True)
 
@@ -33,12 +34,9 @@ sound_rand = pygame.mixer.Sound("sound/Rand.mp3")
 sound_tor = pygame.mixer.Sound("sound/Tor.mp3")
 
 def reset_ball():
-    global ball_pos_x, ball_pos_y, ball_direction_x, ball_direction_y
-    ball_pos_x = (WIDTH - ball_size) // 2
-    ball_pos_y = (HEIGHT - ball_size) // 2
-    ball_direction_x = random.choice([-4, 4])
-    ball_direction_y = random.choice([-4, 4])
-    pygame.time.delay(1000)  # Pause for a moment before resuming
+    global ball_vel, ball
+    ball.center = (WIDTH // 2, HEIGHT // 2)
+    ball_vel = Vector2(1, 0).rotate(random.uniform(-45, 45)) * 5
 
 run = True
 while run:
@@ -46,19 +44,6 @@ while run:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             run = False
-
-    ball_pos_x += ball_direction_x
-    ball_pos_y += ball_direction_y
-    
-    if ball_pos_x < -ball_size:
-        score[1] += 1
-        pygame.mixer.Sound.play(sound_tor)
-        reset_ball()
-
-    if ball_pos_x > WIDTH:
-        score[0] += 1
-        pygame.mixer.Sound.play(sound_tor)
-        reset_ball()
 
     keys = pygame.key.get_pressed()  # Get the state of all keyboard buttons
 
@@ -80,31 +65,52 @@ while run:
     if keys[pygame.K_ESCAPE]:
         run = False
 
+    # Bewegung
+    ball.x += ball_vel.x
+    ball.y += ball_vel.y
+
+    # Wandkollision oben/unten
+    if ball.top <= 0 or ball.bottom >= 600:
+        ball_vel = ball_vel.reflect(Vector2(0, 1))
+        pygame.mixer.Sound.play(sound_rand)
+
+    # Paddle collision
+    if ball.colliderect(player1) and ball_vel.x < 0 and ball.left >= player1.right - 10:
+        hit_pos = ball.centery - player1.centery
+        hit_norm = hit_pos / (player1.height / 2)
+        ball_vel = ball_vel.reflect(Vector2(1, 0))
+        angle_change = hit_norm * 45
+        ball_vel = ball_vel.rotate(angle_change)
+        ball_vel = ball_vel.normalize() * 5
+        pygame.mixer.Sound.play(sound_ping)
+
+    if ball.colliderect(player2) and ball_vel.x > 0 and ball.right <= player2.left + 10:
+        hit_pos = ball.centery - player2.centery
+        hit_norm = hit_pos / (player2.height / 2)
+        ball_vel = ball_vel.reflect(Vector2(-1, 0))
+        angle_change = hit_norm * 45
+        ball_vel = ball_vel.rotate(-angle_change)
+        ball_vel = ball_vel.normalize() * 5
+        pygame.mixer.Sound.play(sound_pong)
+    
+    if ball.x < -ball_size:
+        score[1] += 1
+        pygame.mixer.Sound.play(sound_tor)
+        reset_ball()
+
+    if ball.x > WIDTH:
+        score[0] += 1
+        pygame.mixer.Sound.play(sound_tor)
+        reset_ball()
+
     screen.fill((0, 0, 0))  # Fill the screen with black
     score_screen = font.render(f"{score[0]}   {score[1]}", True, (255, 255, 255))
     screen.blit(score_screen, ((WIDTH - score_screen.get_width()) // 2, 20))
     pygame.draw.line(screen, (255, 255, 255), (WIDTH // 2, 0), (WIDTH // 2, HEIGHT), 3)  # Draw the center line
     player1 = pygame.draw.rect(screen, (255, 255, 255), (border_left_right, player1_pos, player_thickness, player1_hight))
     player2 = pygame.draw.rect(screen, (255, 255, 255), (WIDTH - border_left_right - player_thickness, player2_pos, player_thickness, player2_hight))
-    ball = pygame.draw.rect(screen, (255, 255, 255), (ball_pos_x, ball_pos_y, ball_size, ball_size))  # Draw the ball
-
-    if ball_pos_y <= 0 or ball_pos_y >= HEIGHT - ball_size:
-        ball_direction_y = -ball_direction_y
-        pygame.mixer.Sound.play(sound_rand)
+    pygame.draw.rect(screen, (255, 255, 255), ball)
     
-    if ball.colliderect(player1):
-        if ball_pos_x < border_left_right + player_thickness - 1:  # Prevent multiple collisions
-            pass
-        else:
-            ball_direction_x = -ball_direction_x
-            pygame.mixer.Sound.play(sound_ping)
-    if ball.colliderect(player2):
-        if ball_pos_x > WIDTH - border_left_right - player_thickness - ball_size + 1:  # Prevent multiple collisions
-            pass
-        else:
-            ball_direction_x = -ball_direction_x
-            pygame.mixer.Sound.play(sound_pong)
-
     pygame.display.flip()  # Update the display
 
 pygame.quit()
